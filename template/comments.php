503b62561f69c5cd0e6f853f1c2fdbbc6b8395bc
cosmosfarm <cosmosfarm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 05:31:30 +0000

    수정
    git-svn-id: https://plugins.svn.wordpress.org/cosmosfarm-comments/trunk@1125063 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- template/comments.php
+++ template/comments.php
@@ -2,9 +2,11 @@
 <?php
 global $post;
 $core = new Cosmosfarm_Comments_Core();
+if(comments_open($post->ID)):
 ?>
-<div id="comments" class="comments-area">
+<div id="comments" <?php comment_class('comments-area', null, $post->ID, true)?>>
 	<!-- 코스모스팜 댓글 플러그인 시작 -->
 	<div id="cosmosfarm-comments" data-plugin-id="<?php echo get_option('cosmosfarm_comments_plugin_id', '')?>" data-href="<?php echo get_permalink($post->ID)?>" data-width="100%" data-row="<?php echo get_option('cosmosfarm_comments_plugin_row', '10')?>" data-access-token="<?php echo $core->get_access_token()?>"><a href="http://www.cosmosfarm.com/plugin/comments">코스모스팜 댓글 플러그인</a></div>
 	<!-- 코스모스팜 댓글 플러그인 종료 -->
 </div>
+<?php endif?>
